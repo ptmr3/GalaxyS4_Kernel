669215d1cfbf5024e3476bf935aaf743ace7de59
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-05-17 23:06:10 +0300

    Hotplug

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -424,6 +424,8 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+extern struct cpufreq_governor cpufreq_gov_hotplug;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hotplug)
 #endif
 
 
